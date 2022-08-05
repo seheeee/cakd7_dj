b7b3adf3b18e8afd8a4a285ecff2a90d13052382
seheeee <qawsed4817@gmail.com>
2022-08-05 09:51:11 +0900

    블로그

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+class PostList(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    ordering = '-pk'
+
+class PostDetail(DetailView):
+    model = Post
